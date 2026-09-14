3494676bac7b41383de06da29a3cb3104eddbaf6
kranthi6300 <98908652+kranthi6300@users.noreply.github.com>
2026-09-14 14:09:05 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 const pdfLinks = {
   concept: {
     '1': 'https://kranthi6300.github.io/MathwithK.github.io/All%20maths%20concepts%20arihant%20.pdf',
-    'Concept Type 1': 'https://kranthi6300.github.io/MathwithK.github.io/All%20maths%20concepts%20arihant%20.pdf'
+    'Concept Type 1': 'All maths concepts arihant .pdf'
   }
 };
 
